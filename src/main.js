5baea56227f2480b993053895256fa26cba9b03f
gh <guohui13@163.com>
2020-05-14 02:45:43 +0800

    add by gh

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router  from "@/router";
+import axios from 'axios'
 
 Vue.config.productionTip = false
 
@@ -8,3 +9,7 @@ new Vue({
   router:router,
   render: h => h(App)
 }).$mount('#app')
+
+axios({
+  url:'https://suggest.taobao.com/sug?code=utf-8&q=%E5%8D%AB%E8%A1%A3&callback=cb'
+}).then(res=>{console.log(res)}) //then方法完后就会拿到result，然后打印
